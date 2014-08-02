6e200f047b9fd535f1257ba316f45bacbd948696
Hunor Kovács <kovacshuni@yahoo.com>
2014-08-02 19:41:49 +0300

    Sending through even not authenticated requests. They will just have other headers, so the targeted server can act accordingly.

--- src/main/scala/com/hunorkovacs/koauthproxyfinagle/KoauthFilter.scala
+++ src/main/scala/com/hunorkovacs/koauthproxyfinagle/KoauthFilter.scala
@@ -1,16 +1,26 @@
 package com.hunorkovacs.koauthproxyfinagle
 
+import com.hunorkovacs.koauth.domain.{ResponseBadRequest, ResponseUnauthorized}
 import com.hunorkovacs.koauth.service.provider.persistence.{Persistence, ExampleMemoryPersistence}
 import com.hunorkovacs.koauth.service.provider.ProviderServiceFactory
 import com.twitter.finagle.{Service, Filter}
 import com.twitter.util.{Promise, Future}
 import org.jboss.netty.handler.codec.http.{HttpResponse, HttpRequest}
+import org.jboss.netty.handler.codec.http.HttpResponseStatus.{UNAUTHORIZED, BAD_REQUEST}
 
 import scala.concurrent.ExecutionContext
 
 trait SimpleFilter[Req, Rep] extends Filter[Req, Rep, Req, Rep]
 
 class KoauthFilter() extends SimpleFilter[HttpRequest, HttpResponse] {
+
+  private val HeaderAuthenticated = "x-authenticated"
+  private val HeaderAuthenticatedMethod = "x-authentication-method"
+  private val HeaderOauth1 = "oauth1"
+  private val HeaderUnauthenticatedCode = "x-unauthenticated-code"
+  private val HeaderUnauthenticatedMessage = "x-unauthenticated-message"
+  private val Unauthorized = UNAUTHORIZED.getCode
+  private val BadRequest = BAD_REQUEST.getCode
 
   private implicit val ec = ExecutionContext.Implicits.global
   private implicit val persistence: Persistence = new ExampleMemoryPersistence()
@@ -19,14 +29,23 @@ class KoauthFilter() extends SimpleFilter[HttpRequest, HttpResponse] {
   def apply(request: HttpRequest, service: Service[HttpRequest, HttpResponse]): Future[HttpResponse] = {
     val eitherSF = NettyRequestMapper.map(request)
       .flatMap(oauthService.oauthenticate)
-    val eitherF = scalaFuturetoTwitterFuture(eitherSF)
 
-    eitherF flatMap {
+    scalaFuturetoTwitterFuture(eitherSF) flatMap {
       case Left(nok) =>
-        val nettyResponseSF = NettyResponseMapper.map(nok)
-        scalaFuturetoTwitterFuture(nettyResponseSF)
+        val filteredRequest = nok match {
+          case ResponseUnauthorized(message) =>
+            request.addHeader(HeaderUnauthenticatedCode, Unauthorized)
+            request.addHeader(HeaderUnauthenticatedMessage, message)
+            request
+          case ResponseBadRequest(message) =>
+            request.addHeader(HeaderUnauthenticatedCode, BadRequest)
+            request.addHeader(HeaderUnauthenticatedMessage, message)
+            request
+        }
+        service(filteredRequest)
       case Right(username) =>
-        request.addHeader("x-authenticated", username)
+        request.addHeader(HeaderAuthenticated, username)
+        request.addHeader(HeaderAuthenticatedMethod, HeaderOauth1)
         service(request)
     }
   }

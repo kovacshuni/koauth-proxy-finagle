c08187c98aff17e9f9a89c92448e81a1a52f31fa
Hunor Kovács <kovacshuni@yahoo.com>
2014-08-02 19:03:17 +0300

    Parametrized the proxied server's address.

--- src/main/scala/com/hunorkovacs/koauthproxyfinagle/Proxy.scala
+++ src/main/scala/com/hunorkovacs/koauthproxyfinagle/Proxy.scala
@@ -6,10 +6,13 @@ import org.jboss.netty.handler.codec.http._
 
 object Proxy extends App {
 
+  private val hostname =
+    if (args.length > 0) args(0)
+    else "www.google.com:80"
   private val koauthFilter = new KoauthFilter()
 
   val client: Service[HttpRequest, HttpResponse] =
-    koauthFilter andThen Http.newService("localhost:9000")
+    koauthFilter andThen Http.newService(hostname)
     
   val server = Http.serve(":8080", client)
   Await.ready(server)

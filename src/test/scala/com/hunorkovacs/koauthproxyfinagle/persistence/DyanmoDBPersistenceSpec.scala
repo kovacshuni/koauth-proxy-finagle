74bd4ad9369dd29191c241fe640586c09b3a2a58
Hunor Kovács <kovacshuni@yahoo.com>
2014-08-03 23:24:56 +0300

    Abstract test for Persistence is moved out. Only relevant DyanmoTest is here.

--- src/test/scala/com/hunorkovacs/koauthproxyfinagle/persistence/DyanmoDBPersistenceSpec.scala
+++ src/test/scala/com/hunorkovacs/koauthproxyfinagle/persistence/DyanmoDBPersistenceSpec.scala
@@ -1,8 +1,7 @@
 package com.hunorkovacs.koauthproxyfinagle.persistence
 
-import com.hunorkovacs.koauth.service.provider.persistence.InMemoryPersistence
+import com.hunorkovacs.koauth.service.provider.persistence.PersistenceSpec
+
 import scala.concurrent.ExecutionContext
 
 class DyanmoDBPersistenceSpec extends PersistenceSpec(new DynamoDBPersistence(ExecutionContext.Implicits.global))
-
-class InMemoryPersistenceSpec extends PersistenceSpec(new InMemoryPersistence()(ExecutionContext.Implicits.global))
